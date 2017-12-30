4571b5d1cf5b0a051b7ec83abe78a023ea364ef2
Allen Garvin <aurvondel@gmail.com>
2017-12-29 22:39:06 -0600

    prob 17

--- 17-advent.py
+++ 17-advent.py
@@ -1,27 +1,78 @@
 #!/usr/bin/python
 
-import sys
+import os, sys, itertools, string
+from hashlib import md5
 
-def step(a):
-    return a + "0" + a[::-1].replace("0", "X").replace("1", "0").replace("X", "1")
 
-def cksum(a):
-    return "".join(map(lambda x: "1" if x[0] == x[1] else "0", [a[i:i+2] for i in range(0, len(a), 2)]))
+def door_open(ch):
+    if ch == "b" or ch == "c" or ch == "d" or ch == "e" or ch == "f":
+        return 1
+    return 0
 
-def transform(istr, ilen):
-    while len(istr) < ilen:
-        istr = step(istr)
-    istr = istr[:ilen]
+# Order: up, down, left, right
 
-    c = cksum(istr)
-    while len(c) % 2 == 0:
-        c = cksum(c)
-    return c
+orig_salt = 0
+least_path = 100
+most_path = -1
+winner = None
+loser = None
 
-def main():
-    print "Answer 1:", transform("01000100010010111", 272)
-    print "Answer 1:", transform("01000100010010111", 35651584)
+def display(hash):
+    return "%s%s%s%s" % (("U" if door_open(hash[0]) else "-"), ("D" if door_open(hash[1]) else "-"), ("L" if door_open(hash[2]) else "-"), ("R" if door_open(hash[3]) else "-"))
+
+def go(salt, pos, depth):
+    global orig_salt
+    global least_path, most_path
+    global winner
+
+    if depth > 1000:
+        return
+
+    hash = md5("%s" % (salt)).hexdigest()
+
+    if pos == [3, 3]:
+        #print "WINNER: %d " % (len(salt) - len(orig_salt)), salt[len(orig_salt):]
+        if min(depth, least_path) < least_path:
+            least_path = min(depth, least_path)
+            winner = salt[len(orig_salt):]
+        if (len(salt) - len(orig_salt)) > most_path:
+            most_path = len(salt) - len(orig_salt)
+            loser = salt[len(orig_salt):]
+        return
+
+    dirs = map(door_open, list(hash[0:4]))
+    if pos[0] == 0:
+        dirs[2] = 0
+    if pos[0] == 3:
+        dirs[3] = 0
+    if pos[1] == 0:
+        dirs[0] = 0
+    if pos[1] == 3:
+        dirs[1] = 0
+
+    #print ">" * depth, pos, dirs, salt, hash[0:4], display(hash[0:4]), salt[len(orig_salt):]
+    possibles = "UDLR"
+    possible_deltas = [ [0, -1], [0, 1], [-1, 0], [1, 0] ]
+
+    for d in range(len(dirs)):
+        if dirs[d] == 1:
+            go( salt + possibles[d], [ pos[0] + possible_deltas[d][0], pos[1] + possible_deltas[d][1] ], depth + 1 )
+        
     
+def main(argv):
+    global orig_salt
+
+    if len(argv) != 2:
+        print("Usage: %s salt" % argv[0])
+        sys.exit(1)
+
+    salt = argv[1]
+    orig_salt = salt
+
+    go(salt, [0,0], 1)
+    print "Answer 1:", winner
+    print "Answer 2:", most_path
+    return 1
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
